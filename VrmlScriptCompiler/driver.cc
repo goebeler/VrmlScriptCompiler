7f8449d1e0b15abd1debb21c777190033681684a
Christian Göbeler <goebeler@athel.de>
2020-02-22 16:31:11 +0100

    init

---
+++ VrmlScriptCompiler/driver.cc
@@ -0,0 +1,22 @@
+#include "driver.hh"
+#include "parser.tab.h"
+
+driver::driver()
+	:trace_parsing(true), trace_scanning(true)
+{
+	variables["one"] = 1;
+	variables["two"] = 2;
+}
+
+int driver::parse(const std::string& f)
+{
+	file = f;
+	location.initialize(&file);
+	scan_begin();
+	yy::parser parse(*this);
+	parse.set_debug_level(trace_parsing);
+	int res = parse();
+	scan_end();
+	return res;
+}
+
---
+++ VrmlScriptCompiler/driver.hh
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include "parser.tab.h"
+
+#define YY_DECL yy::parser::symbol_type yylex(driver& drv)
+
+YY_DECL;
+
+class driver
+{
+public:
+	driver();
+
+	std::map<std::string, int> variables;
+	int result;
+
+	int parse(const std::string& f);
+	std::string file;
+	bool trace_parsing;
+
+	void scan_begin();
+	void scan_end();
+
+	bool trace_scanning;
+	yy::location location;
+	
+};
